557e99c54b16626415103b5c1b4c0d35c77dc421
Your Name <sguo35@gmail.com>
2018-10-18 23:46:45 -0700

    feat: implement ProjectGrade updating

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,33 +1,57 @@
-"use strict";
-
-import async from "async";
-import request from "request";
-import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
+import ProjectGrade from "../models/ProjectGrade";
+import { Prompt } from "../models/Prompt";
 
-
+// update form/ProjectGrade w/incomplete results
+interface UpdateProjectGradeRequest extends Request {
+  body: {
+    id: string;
+    prompts: Array<Prompt>;
+  };
+}
 /**
- * GET /api
- * List of API examples.
+ * Updates the given ProjectGrade with the new responses. The client should send the entirety of all responses
+ * for the given ProjectGrade to the server, even if some of them have not changed.
  */
-export let getApi = (req: Request, res: Response) => {
-  res.render("api/index", {
-    title: "API Examples"
+export const updateProjectGrade = async (req: UpdateProjectGradeRequest, res: Response) => {
+  const projectGrade = await ProjectGrade.find({
+    id: req.body.id
+  })
+  .update({
+    responses: req.body.prompts
   });
+  res.status(200).end();
 };
 
+// submit ProjectGrade
+interface SubmitProjectGradeRequest extends Request {
+  body: {
+    id: string; // the ProjectGrade id
+  };
+}
 /**
- * GET /api/facebook
- * Facebook API example.
+ * Submits the final grading for ProjectGrade with id `id` and marks it as done.
  */
-export let getFacebook = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.user.tokens.find((token: any) => token.kind === "facebook");
-  graph.setAccessToken(token.accessToken);
-  graph.get(`${req.user.facebook}?fields=id,name,email,first_name,last_name,gender,link,locale,timezone`, (err: Error, results: graph.FacebookUser) => {
-    if (err) { return next(err); }
-    res.render("api/facebook", {
-      title: "Facebook API",
-      profile: results
-    });
+export const submitProjectGrade = async (req: SubmitProjectGradeRequest, res: Response) => {
+  const projectGrade = await ProjectGrade.find({
+    id: req.body.id
+  }).update({
+    done: true
   });
+  res.status(200).end();
 };
+
+// send grading dashboard emails
+
+// send to everyone
+
+// send to only test email
+
+// send results emails
+
+// populate the spreadsheet w/current results
+
+// get dashboard data
+// own avg, team avg 3 categories
+
+// get ProjectGrade id's for user and project
--- src/models/ProjectGrade.ts
+++ src/models/ProjectGrade.ts
@@ -6,14 +6,18 @@ export type ProjectGradeModel = mongoose.Document & {
   grader: string,
   project: string,
   graded: string,
-  responses: Array<Prompt>
+  responses: Array<Prompt>,
+  done: boolean,
+  id: string
 };
 
 const projectGradeSchema = new mongoose.Schema({
   grader: { type: String },
   project: { type: String },
   graded: { type: String },
-  responses: { type: Array }
+  responses: { type: Array },
+  done: { type: Boolean },
+  id: { type: String }
 }, { timestamps: true });
 
 const ProjectGrade = mongoose.model("ProjectGrade", projectGradeSchema);
