236e98453f0e502a4cb74aaed29f372bba18a0c5
Shiyuan Guo <sguo35@gmail.com>
2018-10-20 20:05:04 -0700

    feat: serve static assets

--- src/app.ts
+++ src/app.ts
@@ -44,9 +44,7 @@ app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 
 
-app.use(
-  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
-);
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 /**
  * Primary app routes.
