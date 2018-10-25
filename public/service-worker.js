efe84127f714d85c42912e1d2b7be77a0b301806
Your Name <sguo35@gmail.com>
2018-10-25 13:22:04 -0700

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17963b50c95b4f6b68ff9ab7873d697d.js"
+  "/precache-manifest.121ff42d5d9567ce231f99d6dd301070.js"
 );
 
 workbox.clientsClaim();
