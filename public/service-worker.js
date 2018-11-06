f0488e8da198c9dca419fb7934d2f4e0ca208d14
Shiyuan Guo <sguo35@gmail.com>
2018-11-06 14:53:49 -0800

    update

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c563902dfd37d950307d9945c9ad3a1.js"
+  "/precache-manifest.8788a53d561448fc39d82a3e4fd2b79b.js"
 );
 
 workbox.clientsClaim();
